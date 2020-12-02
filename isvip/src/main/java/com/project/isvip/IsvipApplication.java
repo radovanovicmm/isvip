fbd9c35b8ea05fda1dd7c737c9874be3524ae70b
Marko Radovanovic <markor97radovanovic@gmail.com>
2020-12-02 13:52:58 +0100

    Setting inital spring project

---
+++ isvip/src/main/java/com/project/isvip/IsvipApplication.java
@@ -0,0 +1,13 @@
+package com.project.isvip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsvipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsvipApplication.class, args);
+	}
+
+}
---
+++ isvip/src/test/java/com/project/isvip/IsvipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.isvip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsvipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
